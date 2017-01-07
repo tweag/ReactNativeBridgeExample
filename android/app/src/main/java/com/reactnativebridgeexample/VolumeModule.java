db8681788bb7165252a9cf13be74fe4739e72799
Patrick Smith <patrick@promptworks.com>
2017-01-07 15:48:46 -0500

    Add Android method to get system volume

--- android/app/src/main/java/com/reactnativebridgeexample/VolumeModule.java
+++ android/app/src/main/java/com/reactnativebridgeexample/VolumeModule.java
@@ -1,5 +1,8 @@
 package com.reactnativebridgeexample.volume;
 
+import android.content.Context;
+import android.media.AudioManager;
+
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -7,13 +10,22 @@ import com.facebook.react.bridge.ReactMethod;
 
 public class VolumeModule extends ReactContextBaseJavaModule {
     private static final String TAG = "Volume";
+    private AudioManager audio;
 
     public VolumeModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        audio = (AudioManager) reactContext.getSystemService(Context.AUDIO_SERVICE);
     }
 
     @Override
     public String getName() {
         return TAG;
+    }
+
+    @ReactMethod
+    public void getSystemVolume(Callback callback) {
+        int currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+        int maxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        callback.invoke(null, ((float) currentVolume / maxVolume));
     }
 }
