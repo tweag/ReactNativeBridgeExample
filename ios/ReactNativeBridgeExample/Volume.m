8f3638a2b3c153eb7b0c4ee71ada2844e1ca0771
Patrick Smith <patrick@promptworks.com>
2017-01-07 14:52:23 -0500

    Add method to get system volume on ios

--- ios/ReactNativeBridgeExample/Volume.m
+++ ios/ReactNativeBridgeExample/Volume.m
@@ -1,7 +1,13 @@
+#import <AVFoundation/AVFoundation.h>
 #import "Volume.h"
 
 @implementation Volume
 
 RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(getSystemVolume:(RCTResponseSenderBlock)callback) {
+  AVAudioSession *session = [AVAudioSession sharedInstance];
+  callback(@[[NSNull null], @([session outputVolume])]);
+}
 
 @end
